af552d528b3429ee279ddc2819e377aed1007194
Aaron Berns <anberns@gmail.com>
2019-01-05 18:31:44 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -17,14 +17,21 @@ def position_taken?(board, index)
 end
 
 
-# Define display_board that accepts a board and prints
-# out the current state.
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+# code your input_to_index and move method here!
+def input_to_index(user_input)
+  (user_input.to_i) -1 
+end
+
+def move(array, index, character = 'X')
+  array[index] = character
 end
 
 # code the #greeting method here!
