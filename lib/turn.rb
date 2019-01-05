8be9deb73cce796a79cd81cd081393c294df5e16
Aaron Berns <anberns@gmail.com>
2019-01-05 18:34:18 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -38,14 +38,3 @@ end
 def greeting(name)
   puts "Hello #{name}. It's nice to meet you."
 end
-
-
-ask for input
-get input
-convert input to index
-if index is valid
-  make the move for index
-  show the board
-else
-  ask for input again until you get a valid input
-end
