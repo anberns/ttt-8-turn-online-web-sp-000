f3a69e05bf90840ff7c9fc7e4b0c3f1e1348a8fe
Aaron Berns <anberns@gmail.com>
2019-01-05 18:43:51 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,3 +1,17 @@
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  while !valid_move?(board, index)
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+  end
+  move(board, index)
+  display_board(board)
+end
+
+
 # code your #valid_move? method here
 def valid_move?(board, index)
   if index >= 0 && index <= 8 && !position_taken?(board, index)
